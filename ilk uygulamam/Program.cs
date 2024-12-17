97c22734d24b3242b6959e2c1531daca74218f65
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2024-12-17 11:29:17 +0300

    first commit

---
+++ ilk uygulamam/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ilkuygulamam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Senanur Yavuzarslan\nYazılım geliştirme alanında uzmanlaşmak ve bu alanda kariyer yapmak istiyorum. Ayrıca oyun geliştirme hedefime ulaşmak için gerekli yazılım altyapısını oluşturmak istiyorum.");
+
+        }
+    }
+}
